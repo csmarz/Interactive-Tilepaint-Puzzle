7f61b59201f85bbb9ca38e37447c86fdc32e8ae5
Vincentius Arnold <vincentiusarnoldfridolin@gmail.com>
2023-06-26 15:27:58 +0700

    Change Folder Name

--- tilepaint_app/wsgi.py
+++ tilepaint_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for task_13 project.
+WSGI config for tilepaint_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_13.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tilepaint_app.settings')
 
 application = get_wsgi_application()
